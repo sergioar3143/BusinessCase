0238ff510300f4abf0694ad5cd5a4c6649080b3f
sergioar3143 <sergioar98@hotmail.com>
2025-02-16 22:31:52 -0600

    Last version

--- img_comparison.py
+++ img_comparison.py
@@ -3,25 +3,26 @@ import numpy as np
 import cv2
 
 # Cargar el modelo de reconocimiento facial
-model = insightface.app.FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
-model.prepare(ctx_id=0)
+#model = insightface.app.FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
+#model.prepare(ctx_id=0)
 
 # Cargar y procesar las imágenes
-def get_embedding(img_path):
-    img = cv2.imread(img_path)
-    faces = model.get(img)
-    if len(faces) == 0:
-        return None
-    return faces[0].embedding  # Embedding de la cara detectada
+def get_embedding(img):
+	model = insightface.app.FaceAnalysis(name='buffalo_l', providers=['CPUExecutionProvider'])
+	model.prepare(ctx_id=0)
+	faces = model.get(img)
+	if len(faces) == 0:
+		return None
+	return faces[0].embedding  # Embedding de la cara detectada
 
-emb1 = get_embedding("ine3.jpeg")
-emb2 = get_embedding("foto.jpeg")
 
-print(emb1.shape)
-print(emb2.shape)
+def compare(img1,img2):
+	emb1 = get_embedding(img1)
+	emb2 = get_embedding(img2)
 
-if emb1 is not None and emb2 is not None:
-    similarity = np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2))
-    print(f"Similitud: {similarity}")
-else:
-    print("No se detectaron rostros en una de las imágenes.")
+	if emb1 is not None and emb2 is not None:
+		similarity = np.dot(emb1, emb2) / (np.linalg.norm(emb1) * np.linalg.norm(emb2))
+		print(f"Similitud: {similarity}")
+	else:
+		print("No se detectaron rostros en una de las imágenes.")
+	return similarity
--- interface.py
+++ interface.py
@@ -5,19 +5,73 @@ from tkinter import filedialog
 from PIL import Image, ImageTk
 from bot_curp import search_curp
 from ocr_curp import find_curp
+from img_comparison import compare
+
+cap = cv2.VideoCapture(0)
+
+def mostrar_video():
+	global cap
+	# Leer el frame actual de la cámara
+	ret, frame = cap.read()
+	# Convertir el frame a una imagen que pueda ser mostrada en la ventana
+	img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	img2 = Image.fromarray(img2)
+	imgtk = ImageTk.PhotoImage(image=img2)
+	# Mostrar la imagen en la ventana
+	label_video.config(image=imgtk)
+	label_video.image = imgtk
+	# Repetir el proceso para mostrar el video en tiempo real
+	frame3.after(10, mostrar_video)
+
+def mostrar_ventana4():
+	global img
+	frame3.pack_forget()
+	ret, frame = cap.read()
+	# Convertir el frame a una imagen que pueda ser mostrada en la ventana
+	img2 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	similarity = compare(img, img2)
+	if similarity>0.35:
+		label = tk.Label(frame4, text='Se confirmó su identidad, comenzando solicitud', font = ('Arial',20))
+		label.pack()
+		frame4.pack(fill="both", expand=True)
+	else:
+		label = tk.Label(frame0, text='No se pudo confirmar su identidad, intente de nuevo ', font = ('Arial',20))
+		label.pack()
+		frame0.pack(fill="both", expand=True)
+
+def mostrar_ventana3():
+	frame2.pack_forget()
+	frame3.pack(fill="both", expand=True)
+
+def mostrar_ventana1():
+    frame0.pack_forget()
+    frame1.pack(fill="both", expand=True)
 
 def mostrar_ventana2():
 	global img,msg
 	frame1.pack_forget()  # Oculta la ventana 1
 	curp=find_curp(img)
 	df=search_curp([curp])
-	msg='Bienvenido '+df.iloc[0,1]
-	label = tk.Label(frame2, text=msg)
+	if  df.iloc[0,4]=='HOMBRE':
+		msg='Bienvenido '+df.iloc[0,1]
+	else:
+		msg='Bienvenida '+df.iloc[0,1]
+	apellido1='Apellido Paterno: '+df.iloc[0,2]
+	apellido2='Apellido Materno: '+df.iloc[0,3]
+	fecha='Fecha de Nacimiento: '+df.iloc[0,5]
+	#print(df)
+	label = tk.Label(frame2, text=msg, font = ('Arial',20))
 	label.pack()
-	button2 = tk.Button(frame2, text="Cargar imagen",command=lambda:cargar_imagen('Cargar imagen',label_imagen2), activebackground="#F50743")
+	labelapellido = tk.Label(frame2, text=apellido1, font = ('Arial',20))
+	labelapellido.pack()
+	labelapellido = tk.Label(frame2, text=apellido2, font = ('Arial',20))
+	labelapellido.pack()
+	labelfecha = tk.Label(frame2, text=fecha, font = ('Arial',20))
+	labelfecha.pack()
+	labelx = tk.Label(frame2, text='Presiona continuar si tus datos son correctos', font=('Arial',20))
+	labelx.pack()
+	button2 = tk.Button(frame2, text="Continuar",command=mostrar_ventana3, activebackground="#F50743")
 	button2.pack()
-	label_imagen2 = tk.Label(frame2, text=msg)
-	label_imagen2.pack()
 	frame2.pack(fill="both", expand=True)  # Muestra la ventana 2
 
 def cargar_imagen(text, label):
@@ -43,17 +97,24 @@ root.title("Validar sujeto")
 frame1 = tk.Frame(root)
 button1 = tk.Button(frame1, text="Cargar INE",command=lambda:cargar_imagen('Cargar INE',label_imagen1), activebackground="#F50743")
 button1.pack()
-
-#button1_1=tk.Button(frame1, text="Continuar", command=mostrar_ventana2).pack()
 label_imagen1 = tk.Label(frame1, text='INE')
 label_imagen1.pack()
-
-button1_1=tk.Button(frame1, text="Continuar", command=mostrar_ventana2, activebackground="#F50743").pack()
-
-
+button1_1 = tk.Button(frame1, text="Continuar", command=mostrar_ventana2, activebackground="#F50743").pack()
 frame1.pack(fill="both", expand=True)  # Mostrar al inicio
 
 ##Window 2
 frame2 = tk.Frame(root)
+
+##Window 3
+frame3 = tk.Frame(root)
+button3 = tk.Button(frame3, text="Abrir camara", command=mostrar_video, activebackground="#F50743").pack()
+button3 = tk.Button(frame3, text="Tomar foto", command=mostrar_ventana4, activebackground="#F50743").pack()
+label_video = tk.Label(frame3)
+label_video.pack()
+
+frame0 = tk.Frame(root)
+button0 = tk.Button(frame0, text="Continuar", command=mostrar_ventana1, activebackground="#F50743").pack()
+
+frame4 = tk.Frame(root)
 
 root.mainloop()
